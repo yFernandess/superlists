2a085ac23394480476b6a22474a4fdb5904a207b
yuri.fernandes <yuri.fernandes@softplan.com.br>
2018-05-10 08:31:34 -0300

    Created a new test for functional_tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -11,10 +14,73 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row_text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:        
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except(AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    def test_can_start_a_list_for_one_user(self):
+        # Edith ouviu falar de uma nova aplicacao online interessante para
+        # lista de tarefas. Ela decide verificar sua homepage
+        self.browser.get(self.live_server_url)
+
+        # A pagina eh atualizada novamente e agora mostra os dois itens em sua lista
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # Satisfeita ela volta a dormir
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith inicia uma nova lista de tarefas
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        #Ela percebe que sua lista tem um URL unico
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+') 
+
+        # Agora um novo usuario, Francis, chega ao site.
+        
+        ## Usamos uma nova sessao de navegador para garantir que nenhuma informacao
+        ## de Edith esta vindo de cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis acessa a pagina inicial. Nao ha nenhum sinal da lista de Edith
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)        
+
+        # Francis inicia uma nova lista inserindo um item novo. Ele
+        # eh menos interessante que Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Francis obtem seu proprio URL exclusivo
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Novamente, nao ha nenhum sinal da lista de Edith
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfeitos, ambos voltam a dormir
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith ouviu falar de uma nova aplicacao online interessante para
@@ -42,7 +108,7 @@ class NewVisitorTest(LiveServerTestCase):
         # "1: Buy peacock feathers" como um item em uma lista de tarefas
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -57,8 +123,8 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(1)
 
         # A pagina eh atualizada novamente e agora mostra os dois itens em sua lista
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
         # Edith se pergunta se o site lembrara de sua lista. Entao ela nota
         # que o site gerou um URL unico para ela -- ha um pequeno
