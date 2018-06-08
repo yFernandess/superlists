4a4bf3d6f2b3ecffbd9a05720eaa47efddd70ffa
yuri.fernandes <yuri.fernandes@softplan.com.br>
2018-06-08 08:32:58 -0300

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -137,3 +137,28 @@ class NewVisitorTest(LiveServerTestCase):
         # Ela acessa esse URL - sua lista de tarefas continua la.
 
         # Satisfeita, ela volta a dormir
+
+    def test_layout_and_Styling(self):
+        # Edith acessa a página inicial
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Ela percebe que a caixa de entrada está elegantemente centralizada
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # Ela inicia uma nova lista e vê que a entrada está elegantemente
+        # centralizada aí também
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
